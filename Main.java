aef7207c9f60b56da2a08da0ddd8c5d340c58c1b
indirajaD <indhudevareddy@gmail.com>
2020-04-04 10:52:34 +0530

    default user comment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("welcome to git dev branch");
+        System.out.println("welcome to git default user");
     }
 }
